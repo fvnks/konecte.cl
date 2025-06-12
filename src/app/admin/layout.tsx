42c1b54b5b96413e5401ee56ccd4e78aaf54c1be
Rodrigo Andres Droguett Stahr <andres.stahr@gmail.com>
2025-06-12 23:33:19 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,5 @@
 
-import type { ReactNode } from 'react';
+import React, { type ReactNode } from 'react';
 import Link from 'next/link';
 import { Home, Settings, Users, LayoutDashboard, ShieldAlert, CreditCard, ListOrdered, Brush, FileSearch, LogOut } from 'lucide-react';
 import { Button } from '@/components/ui/button';
