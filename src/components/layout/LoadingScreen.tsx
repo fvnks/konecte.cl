6c6ebf83167f78719ea7442ca010d38eaf2b29ce
Rodrigo Andres Droguett Stahr <andres.stahr@gmail.com>
2025-06-23 18:47:33 +0000

    cambia la animacion de carga con esta en todo el sitio:
    import React fr

--- src/components/layout/LoadingScreen.tsx
+++ src/components/layout/LoadingScreen.tsx
@@ -4,11 +4,8 @@ import CustomPageLoader from '@/components/ui/CustomPageLoader';
 
 export default function LoadingScreen() {
   return (
-    <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-white">
+    <div className="fixed inset-0 z-[9999] flex flex-col items-center justify-center bg-white dark:bg-black">
       <CustomPageLoader />
-      <p className="text-zinc-600 text-lg font-medium absolute top-[calc(50%+120px)]">
-        Cargando Konecte...
-      </p>
     </div>
   );
 }
